08554bffb1c1f7d8054ab8a04caea98b18c08f58
4lokym <mykola.koshilko@gmail.com>
2025-05-04 09:58:01 +0200

    test

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ import "./style.css";
 
 class Option {
   constructor(textContent, action) {
-    this.textContent = textContent;
-    this.action = action;
+    this._textContent = textContent;
+    this._action = action;
   }
   get textContent() {
-    return this.textContent;
+    return this._textContent;
   }
   get action() {
-    return this.action;
+    return this._action;
   }
 }
 
 const dropDown = function activateDropDown(
   anchor,
   options,
+  gap,
   x_offset = 0,
   y_offset = 0,
 ) {
@@ -28,16 +29,26 @@ const dropDown = function activateDropDown(
   point.classList.add("point");
 
   const reference = anchor.getBoundingClientRect();
-  const x = reference.left + reference.width / 2 + x_offset;
-  const y = reference.top + y_offset + reference.height;
+  // const x = reference.left + reference.width / 2 + x_offset;
+  // const y = reference.top + y_offset + reference.height;
+  const x = (reference.width / 2) + "px";
 
   point.style.left = x + "px";
-  point.style.top = y + "px";
+  point.style.top =  "100px";
+  point.style.width = reference.width + 'px';
 
-  document.querySelector("body").append(point);
-  
+  options.map(function createAndAddAnOptionOnTheDropDownMenu(option){
+    const button = document.createElement("button");
+    button.classList.add("option");
+    button.textContent = option.textContent;
+    button.addEventListener("click", option.action);
+  });
+
+  anchor.append(point);
 };
 
+const testOption = new Option("test", () => alert("ciao"));
+
 document.querySelector(".options").addEventListener("click", (event) => {
-  dropDown(event.target);
+  dropDown(event.target, [testOption]);
 });
